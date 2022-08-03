66d3aa13a0d7ab5d64320a15f0d31ffbbc487b08
sonnt0201 <nguyenthaison0201@gmaill.com>
2022-08-03 10:40:32 +0700

    init

--- News App/ViewController.swift
+++ News App/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+        title = "News"
+        
         // Do any additional setup after loading the view.
     }
 
