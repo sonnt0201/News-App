ca656abd88a52c254f2394cb97b98084454cf891
sonnt0201 <nguyenthaison0201@gmaill.com>
2022-08-03 10:30:52 +0700

    init

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Sơn Nguyễn on 03/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
